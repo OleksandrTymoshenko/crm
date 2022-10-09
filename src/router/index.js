02ec53c3cfda42c3802858a0a373e09c51b1180e
OleksandrTymoshenko <nomakievip@gmail.com>
2022-10-09 17:19:49 +0300

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/Home-view.vue";
 
 const routes = [
   {
@@ -8,13 +8,58 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login-view.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register-view.vue"),
+  },
+  {
+    path: "/",
+    name: "home",
+    meta: { layout: "main" },
+    component: () => import("../views/Home-view.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories-view.vue"),
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail-view.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History-view.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning-view.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile-view.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record-view.vue"),
   },
 ];
 
